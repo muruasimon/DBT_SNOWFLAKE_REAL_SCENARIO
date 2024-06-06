2e33dc3af6aca9334320cad9c164d0e8c7a5504c
fliamurua cbabkp <fliamuruacbabkp@gmail.com>
2024-06-06 23:18:00 +0000

    14

--- models/staging/stg_products.sql
+++ models/staging/stg_products.sql
@@ -3,13 +3,13 @@ with
         select
             p_partkey as product_id,
             p_name as product_name,
-            p_mfgr as manufacturer
-            p_category as category,
+            p_mfgr as manufacturer,
+           -- p_category as category,
             p_brand as brand,
             p_type as type,
             p_size as size,
             p_container as container
-        from snowflake_sample_data.tpch_sf1.part
+       -- from snowflake_sample_data.tpch_sf1.part
     )
 
 select
