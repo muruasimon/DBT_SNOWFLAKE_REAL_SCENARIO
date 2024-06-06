fdb27113a1034d1a8d7ad211666005b98e6bd93d
fliamurua cbabkp <fliamuruacbabkp@gmail.com>
2024-06-06 20:40:06 +0000

    9

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,18 @@
+with raw_orders as (
+    select
+        O_ORDERKEY as order_id,
+        O_CUSTKEY as customer_id,
+        O_ORDERSTATUS as order_status,
+        O_TOTALPRICE as total_price,
+        O_ORDERDATE as order_date
+    from SNOWFLAKE_SAMPLE_DATA.TPCH_SF1.ORDERS
+)
+
+select
+    order_id,
+    customer_id,
+    order_status,
+    total_price,
+    order_date
+from raw_orders
+where order_date >= '2023-01-01'
